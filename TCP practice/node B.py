03f552c8187d17ccec6d0129c768daead5ae5e7e
LinsuXia <xialinsu.globe@gmail.com>
2024-04-01 09:57:43 +0800

    一些改动

--- TCP practice/node B.py
+++ TCP practice/node B.py
@@ -8,11 +8,11 @@ HEADER_LENGTH = 15
 IP = "127.0.0.1"
 SERVER_PORT = 2345
 PACKAGE = "hello"
-NODENAME = "nodeA"
-INPUT_PORT1 = "AI1"
-INPUT_PORT2 = "AI2"
-OUTPUT_PORT1 = "AO1"
-OUTPUT_PORT2 = "AO2"
+NODENAME = "nodeB"
+INPUT_PORT1 = "BI1"
+INPUT_PORT2 = "BI2"
+OUTPUT_PORT1 = "BO1"
+OUTPUT_PORT2 = "BO2"
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, SERVER_PORT))
@@ -26,7 +26,7 @@ client_socket.send(username_header + nodeName)
 #main
 while True:
     try:
-        while True: 
+        while True:
             
             data = input("> ")
             if data:
@@ -37,8 +37,9 @@ while True:
                 message = pickle.dumps(message)
                 message_header  = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
                 client_socket.send(message_header + message)
-            
+
             message_header = client_socket.recv(HEADER_LENGTH)
+            
             if not len(message_header):
                 print("connection closed by the server")
                 sys.exit()
@@ -47,12 +48,10 @@ while True:
             recieve_message = pickle.loads(client_socket.recv(message_length))
             beginNode = recieve_message["beginNode"]
             beginPort = recieve_message["beginPort"]
-
             main_message = recieve_message["data"]
 
             print(f"Recieved message from node {beginNode} port{beginPort}: {main_message}")
             time.sleep(5)
-
     except IOError as e:
         if e.errno != errno .EAGAIN and e.errno != errno.EWOULDBLOCK:
             print('Reading error', str(e))
