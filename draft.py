ab4e25556808dd9fb438a3383e157c475e79bf7f
LinsuXia <xialinsu.globe@gmail.com>
2024-04-12 11:20:36 +0800

    重大更新
    基本完成逻辑

---
+++ draft.py
@@ -0,0 +1,11 @@
+t = 10
+
+dic = {
+    "name": t,
+    "sadfa":12345,
+    "sdf": 12313
+}
+
+t += 1
+print(t)
+print(dic[0])
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import socket
-import pickle
 import json
 import threading
+import time
 
 
+lock = threading.Lock()
 
 # ----------------------------------------------------------
 class Node:
-    def __init__(self,node_name:str,input:list,output:list,_socket:socket = None,step_size:str = "0.01",start_time:str = "0"):
+    def __init__(self,node_name:str,input:list,output:list,_socket:socket = None,step_size:float = 0.01,start_time:float = 0.00):
         self.node_name = node_name
         self.input = input
         self.output = output
@@ -16,7 +17,7 @@ class Node:
 
         self.step_size = step_size
         self.curr_time = start_time
-
+        self.send_time = start_time
         #send_data to this Node
         self.input_data = []
         #recv_data from this Node
@@ -44,7 +45,7 @@ class Node:
             self.output_data.clear()
             self.output_data = recv_data[:-1]
 
-        if recv_data[-1]<self.curr_time:
+        if recv_data[-1] < self.curr_time:
             raise Exception("{} recv wrong data".format(self.node_name))
         else:
             self.curr_time = recv_data[-1]
@@ -68,15 +69,17 @@ class Node:
 
     #if socket is loaded return true
     def is_Node_available(self)->bool:
-        if self.socket is not None: 
+        if self.socket is not None:
             return True
         else:
            return False
+    def next_step(self):
+        self.send_time += self.step_size
 
 
 # ----------------------------------------------------------
 class Link:
-    def __init__(self,name:str,begin_node:Node,end_node:Node,begin_port:str,end_port:str):
+    def __init__(self,name:str,begin_node:str,end_node:str,begin_port:str,end_port:str):
         self.link_name = name
         self.begin_node = begin_node
         self.end_node = end_node
@@ -115,23 +118,6 @@ class Link:
                 raise Exception("{}'s end_port is wrong".format(self.link_name))
         else:
             return self.end_port_index
-           
-
-
-nodes = {}
-# ----------------------------------------------------------
-# recv message should be a string
-#接受的信息格式为data1,data2.....,time
-#这个位置我们接受2进制信息的时候长度应该是固定的
-#这个位置还需要后续更新
-def recieve_message(client_socket:socket,standard_length:int = 1024)->str:
-    try:
-        message = client_socket.recv(standard_length).decode('utf-8')
-        message = pickle.loads(message)
-        return message
-        
-    except:
-        return ""
 
 # ---------------------------------------------------------------------------------
 #linking function, target of main thread
@@ -141,21 +127,41 @@ def recieve_message(client_socket:socket,standard_length:int = 1024)->str:
 #第一个函数是在初始化完link_dict之后，我们直接开启link_num个线程，用于更新beginport->endport的信息传输
 #在这个函数while条件里写上，当且仅当beginNode.scoket is not None and endNode.socket is not None 才开始起作用
 #第二个函数是在收到一个accept一个node之后，开启一个线程用来接收这个node从client传入的信息
-def linking(beginNode_socket, endPort_socket):
-    global nodes
+def link_thread(link:Link):
+    #这一部分由夏琳苏负责
+    global node_dict
     while True:
-        #recieve
-        message = recieve_message(beginNode_socket)
-        if message is False:
-            print(f"Closed connction from {nodes[beginNode_socket]}")
-        nodeName = nodes[beginNode_socket]
-        for check_socket in nodes:
-            if (check_socket.getpeername()[0] == endPort_socket.getpeername()[0]) and (check_socket.getpeername()[1] == endPort_socket.getpeername()[1]):
-                #add info and send
-                message["beginNode"] = nodeName
-                send_message = pickle.dumps(message)
-                print(f"Sending message from {nodeName} {beginNode_socket.getpeername()} to {nodes[check_socket]} {check_socket.getpeername()}")
-                check_socket.send(f"{len(send_message):<{HEADER_LENGTH}}".encode("utf-8") + send_message)
+        beginnode_socket = Node(node_dict[link.begin_node]).socket
+        endnode_socket = Node(node_dict[link.end_node]).socket
+        while beginnode_socket != None and endnode_socket != None:
+            beginport_index = link.get_begin_port_index()
+            endport_index = link.get_end_port_index()
+            beginport_sendtime = Node(node_dict[link.begin_node]).curr_time
+            endport_time = Node(node_dict[link.end_node]).curr_time
+            output_lst = Node(node_dict[link.begin_node]).output_data
+            begin_port_crrtime = Node(node_dict[link.begin_node]).curr_time
+            if begin_port_crrtime >= beginport_sendtime:
+                Node(node_dict[link.end_node]).input_data.insert(output_lst[beginport_index-1],endport_index -1)
+                pass
+            continue
+
+def node_thread(nodename:str):
+    global node_dict
+    while True:
+        try:
+            output_message = Node(node_dict[nodename]).socket.recv(1024).decode("utf-8").strip("\x00")
+            node_dict[nodename].refresh_ouput_data(output_message)
+        except Exception as e:
+            print(e)
+        try:
+            input_message = Node[nodename].get_input_data()
+            Node(node_dict[nodename]).socket.send(input_message.ljust(1024,"\x00").encode("utf-8"))
+        except Exception as e:
+            print(e)
+        Node(node_dict[nodename]).next_step()
+
+
+    # 这一部分由陈若涵负责
 
 # --------------------------------------------------------------------------------------
 
@@ -178,28 +184,18 @@ def json_data_preprocess(settings):
         node_dict[node_lists[i]["nodeName"]] = Node(node_lists[i]["nodeName"],node_lists[i]["input"],node_lists[i]["output"])
 
     for i in range(int(link_num)):
-        begin_node = node_dict.get(port2node(link_lists[i]["beginPort"],True))
-        end_node = node_dict.get(port2node(link_lists[i]["endPort"]))
+        begin_node = port2node(link_lists[i]["beginPort"])
+        end_node = port2node(link_lists[i]["endPort"])
         if begin_node is not None and end_node is not None:
             link_dict[link_lists[i]["Name"]] = Link(link_lists[i]["Name"],begin_node,end_node,link_lists[i]["beginPort"],link_lists[i]["endPort"])
 
     return ip,port,node_dict,link_dict
 
-def port2node(port:str,is_begin_port:bool = False)->str:
-    if is_begin_port:
-        cnt:int = 0
-        for i in range(len(port)-1, -1, -1):
-            cnt += 1
-            if port[i] == 'O':
-                break   
-        return "node"+port[:-cnt]
-    else:
-        cnt:int = 0
-        for i in range(len(port)-1, -1, -1):
-            cnt += 1
-            if port[i] == 'I':
-                break   
-        return "node"+port[:-cnt]
+def port2node(port:str,)->str:
+    #这一部分的函数由张宸凯负责优化
+    node_name = "nodeA"
+    return node_name
+    
 
 # --------------------------------------------------------------------------------------
 #accept new node
@@ -207,24 +203,25 @@ def main(settings_file_path:str = "settings.json"):
     #json file load
     with open(settings_file_path, 'r') as file:
         settings = json.load(file)
-    
     #data preprocess
+    global node_dict, link_dict
     ip,port,node_dict,link_dict = json_data_preprocess(settings)
     #server socket init and wait for client
     server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server_socket.bind((ip, port))
     server_socket.listen(0)
-    print("Service started, waiting for client")
+    print("Server started, waiting for client")
 
     while True:
         node_socket, node_address = server_socket.accept()
         #收到的第一条信息为这个节点的名字
         #这里的"\x00"是填充信息，因为.cpp,.c的send的长度是一个定长，需要用\x00填充到固定长度，可以先不用管这一行
-        node_name = node_socket.recv(1024).decode('utf-8').split("\x00")[0]
+        node_name = node_socket.recv(1024).decode('utf-8').strip("\x00")
         print("{} connected".format(node_name))
         
         if(node_name in node_dict.keys()):
             node_dict[node_name].load_socket(node_socket)
+
         else:
             node_socket.close()
             print("Unknown client rejected!")
@@ -235,4 +232,4 @@ def main(settings_file_path:str = "settings.json"):
 
 # ----------------------------------------------------------------------------------------------------
 if '__main__' == __name__:
-    main("sample1.json")
+    main("settings.json")
